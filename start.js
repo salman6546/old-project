2a15f49da1345b5a5f186e2b125f82e6445612cf
salman6546 <imdsalman772@gmail.com>
2024-09-06 10:55:13 -0700

    old-project

---
+++ start.js
@@ -0,0 +1,18 @@
+let a = 10
+let b = 15
+let c = a + b
+
+
+
+
+// console.log("10 + 15 = " + (10 + 15))
+// console.log("10 - 15 = " + (10 - 15))
+// console.log("10 * 15 = " + (10 * 15))
+// console.log("10 / 15 = " + (10 / 15))
+
+
+console.log( a + " + " + b + " = " + c)
+console.log(a + " - "+ b + " = " +(a - b) )
+console.log(a + " * "+ b + " = " +(a * b) )
+console.log(a + " / "+ b + " = " +(a / b) )
+console.log(a+" % "+b+ " = " +(a % b))
---
+++ startTWO.js
@@ -0,0 +1,16 @@
+let a = 10
+let b = 15
+let c = "Hello"
+let d = 'Hello'
+let e = true
+let f
+let g = null
+
+
+console.log(typeof a)
+console.log( typeof b)
+console.log( typeof c)
+console.log( typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
